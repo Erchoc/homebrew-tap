c1727516f66f1ed954e897340a4f7621944012f0
longye <erchoc@qq.com>
2026-04-18 10:34:45 +0800

    chore(cb): bump to v0.1.0-beta.5

--- Formula/cb.rb
+++ Formula/cb.rb
@@ -1,30 +1,30 @@
 class Cb < Formula
   desc "Cross-platform voice assistant that lives in your terminal"
   homepage "https://github.com/Erchoc/chatbot"
-  version "0.1.0-beta.4"
+  version "0.1.0-beta.5"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/Erchoc/chatbot/releases/download/v#{version}/cb-macos-universal"
-      sha256 "60ec5818a11113666a24fdd45e22523055006d73e581a31a302ba2efd3f655e4"
+      sha256 "6e399d8ab272bbcaa0adacf3dbaac63111b3631612061a9a7d70244ce3e3b2be"
     end
 
     on_intel do
       url "https://github.com/Erchoc/chatbot/releases/download/v#{version}/cb-macos-universal"
-      sha256 "60ec5818a11113666a24fdd45e22523055006d73e581a31a302ba2efd3f655e4"
+      sha256 "6e399d8ab272bbcaa0adacf3dbaac63111b3631612061a9a7d70244ce3e3b2be"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/Erchoc/chatbot/releases/download/v#{version}/cb-linux-x86_64"
-      sha256 "9de94be549bc01e2162a45a049d5761d5cfa76f78b1d7eb089dd62c7e7a792bf"
+      sha256 "e151ff1714716511f465b3e6ccb8d29997a53d121f0a55019f7db30d7cd04898"
     end
 
     on_arm do
       url "https://github.com/Erchoc/chatbot/releases/download/v#{version}/cb-linux-arm64"
-      sha256 "19e6d1acd8acf5f10d15d58ff6a573efec2adccfbfd8e700d7911c1a4931e6ff"
+      sha256 "a06427d656d8bfc3a2c68be49299d85e1f7ce0a3dbe3c4b24ca99634ec59a48c"
     end
   end
 
